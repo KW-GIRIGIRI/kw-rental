ca02789dcdd3e72a90bc84c471105a0bcbad67e8
ekdms5566 <ekdms5566@naver.com>
2023-02-22 23:33:39 +0900

    Feat : GlobalStyle, ThemeProvider 적용
    globalStyle 생성
    theme 파일 생성 및 ThemeProvider 적용

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  input {
+    outline: none;
+  }
+
+  .ir {
+      width: 1px;
+      height: 1px;
+      margin: -1px;
+      position: absolute;
+      overflow: hidden;
+      clip: rect(1px 1px 1px 1px);
+    }
+`;
