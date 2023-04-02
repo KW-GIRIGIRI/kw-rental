9a851a48cf4a11d1f89c2fce1592ecb9e066b28e
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-02 23:17:01 +0900

    Feat : 품목 컴포넌트 수정

--- src/components/ItemManagerWrap/ItemManager/index.jsx
+++ src/components/ItemManagerWrap/ItemManager/index.jsx
@@ -1,10 +1,10 @@
 import * as S from "./style";
 
-export default function ItemManager({ id }) {
+export default function ItemManager() {
   return (
     <>
       <S.ItemWrap>
-        <S.p>품목 {id}</S.p>
+        <S.p>품목</S.p>
         <S.InputId placeholder="재물번호를 입력하세요." />
       </S.ItemWrap>
     </>
--- src/components/ItemManagerWrap/index.jsx
+++ src/components/ItemManagerWrap/index.jsx
@@ -1,11 +1,11 @@
 import * as S from "./style";
 import ItemManager from "./ItemManager";
 import { useState } from "react";
+import ItemList from "../ItemListWrap/ItemList";
 import iconPlus from "../../assets/icon-plus-mono.svg";
 import iconMinus from "../../assets/icon-minus-circle-mono.svg";
 
-export default function ItemManagerWrap() {
-  // API 연동 안 하고 임의로 만든 데이터
+export default function ItemManagerWrap({ item, isEdit }) {
   const existingData = [
     {
       id: 1,
@@ -14,18 +14,14 @@ export default function ItemManagerWrap() {
   ]
   const [data, setData] = useState(existingData);
   const [idx, setIdx] = useState(data.at(-1).id);
-  // const [num, setNum] = useState(existingData.at(-1).자산번호);
-  // const [input, setInput] = useState('');
 
   const addItem = () => {
     setIdx(data.at(-1).id + 1);
     const item = {
       id: idx + 1,
-      // 자산번호: num + 1
     }
 
     setData(data.concat(item));
-    // setNum(num + 1);
   }
 
   const delItem = () => {
@@ -37,22 +33,46 @@ export default function ItemManagerWrap() {
 
   return (
     <>
-      <S.Div>
-        <S.ItemUl>
-          {data.map(i => {
-            return (
-              <S.ItemLi key={i.id}>
-                <ItemManager delItem={delItem} id={i.id} />
-                <S.DelBtn onClick={() => { delItem(i.id) }}><S.MinusImg src={iconMinus} /></S.DelBtn>
+      {isEdit ? (
+        <S.Div>
+          {item ? (
+            <S.ItemUl>
+              {item.items.map((i) => (
+                <ItemList id={i.id - item.items[0].id + 1} num={i.propertyNumber} isEdit={isEdit}>
+                  <ItemManager delItem={delItem} />
+                  <S.DelBtn onClick={() => delItem(i.id)}>
+                    <S.MinusImg src={iconMinus} />
+                  </S.DelBtn>
+                </ItemList>
+              ))}
+              <S.ItemLi>
+                <S.AddBtn onClick={addItem}>
+                  <S.PlusImg src={iconPlus} />
+                  <S.P>New</S.P>
+                </S.AddBtn>
               </S.ItemLi>
-            )
-          })}
-          <S.ItemLi>
-            <S.AddBtn onClick={addItem}><S.PlusImg src={iconPlus} /><S.P>New</S.P></S.AddBtn>
-          </S.ItemLi>
-        </S.ItemUl>
-      </S.Div>
-
+            </S.ItemUl>
+          ) : (
+            <div>Loading...</div>
+          )}
+        </S.Div>
+      ) : (
+        <S.Div>
+          <S.ItemUl>
+            {data.map(i => {
+              return (
+                <S.ItemLi key={i.id}>
+                  <ItemManager delItem={delItem} id={i.id} />
+                  <S.DelBtn onClick={() => { delItem(i.id) }}><S.MinusImg src={iconMinus} /></S.DelBtn>
+                </S.ItemLi>
+              )
+            })}
+            <S.ItemLi>
+              <S.AddBtn onClick={addItem}><S.PlusImg src={iconPlus} /><S.P>New</S.P></S.AddBtn>
+            </S.ItemLi>
+          </S.ItemUl>
+        </S.Div>
+      )}
     </>
   )
 }
