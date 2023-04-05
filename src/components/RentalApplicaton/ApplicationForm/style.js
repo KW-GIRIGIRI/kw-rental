14d540eabe51eda2a5f90eed3a1fbfbac9811886
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-04-05 20:48:21 +0900

    Feat : 대여하기 페이지 컴포넌트 마크업 구현

---
+++ src/components/RentalApplicaton/ApplicationForm/style.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+export const FormWrap = styled.ul`
+  height: 82px;
+  margin-bottom: 15px;
+
+  P, Input, Textarea {
+      box-sizing: border-box;
+      border-radius: ${props => props.theme.borderRadius.lv1};
+      border: 1px solid ${props => props.theme.color.gray.g3};
+      color: ${props => props.theme.color.gray.g3};
+  }
+
+  span {
+    margin: 0 6px;
+  }
+`;
+
+export const Info = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 30px;
+`
+
+export const Purpose = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const LiWrap = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const FormLi = styled.li`
+  list-style: disc;
+  list-style-position : inside;
+  font-size: 14px;
+  color: ${props => props.theme.color.text.black};
+  line-height: 17px;
+  margin-right: 33px;
+`;
+
+export const P = styled.p`
+  width: 115px;
+  height: 27px;
+  font-size: 14px;
+  color: ${props => props.theme.color.gray.g3};
+  background: ${props => props.theme.color.primary.sub};
+  display: flex;
+  justify-content:center;
+  align-items:center;
+`
+
+export const Input = styled.input`
+  width: 115px;
+  height: 27px;
+  text-align: center;
+  font-size: 14px;
+  color: ${props => props.theme.color.gray.g3};
+`;
+
+export const Textarea = styled.textarea`
+  width: 700px;
+  height: 101px;
+  resize: none;
+  padding: 12px 20px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Exclam = styled.div`
+  margin-top:10px;
+
+  & > span {
+    margin-left: 7px;
+    font-size: 11px;
+    color: ${props => props.theme.color.primary.main};
+  }
+
+  display: flex;
+  align-items: center;
+`
+
+export const TextareaWrap = styled.div`
+  
+`
---
+++ src/components/RentalApplicaton/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  width: 820px;
+  height: 480px;
+  margin: 0 auto;
+  border-radius: 10px;
+
+  h1 {
+    font-size: 20px;
+    margin-bottom: 26px;
+  }
+
+  h2 {
+    font-size: 16px;
+    margin-bottom: 16px;
+    margin-top: 30px;
+  }
+`; 
