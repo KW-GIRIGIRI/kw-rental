c9fe5fe5a432c93c55507e45bd30e3fbb6728519
ekdms5566 <ekdms5566@naver.com>
2023-04-07 09:29:00 +0900

    Refactor : 담은 기자재 리스트 아이템 분리

---
+++ src/components/EquipCartList/ListItem/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { category } from "../../../data/category"
+import useModal from "../../../hook/useModal"
+import Button from "../../../modules/Button"
+import Image from "../../../modules/Image"
+import ModifyModal from "../ModifyModal"
+import * as S from "./style"
+
+export default function ListItem({ item }) {
+  const [modal, setModal] = useState(false)
+  const { Modal, open, close } = useModal()
+
+  const handleDeleteInventory = async (id) => {
+  console.log(id)
+  }
+  
+  return (
+    <S.ListLi>
+      <ModifyModal modal={modal} setModal={setModal} />
+      <Modal>
+        <p>정말 삭제하시겠습니까?</p>
+        <div>
+          <Button onClick={close} text='취소' className='sub' padding='11px 34px' borderRadius='5px' />
+          <Button onClick={() => handleDeleteInventory(item.id)} text='삭제' className='main' padding='12px 34px' borderRadius='5px' />
+        </div>
+      </Modal>
+      
+      <Image width="72px" height="72px" borderRadius="10px" src={item.imgUrl} alt='' />
+      <S.ItemWrap>
+        <p>{category.map(value => 
+            value.value === item.category && value.label
+            )}</p>
+        <p>{item.modelName}</p>
+      </S.ItemWrap>
+      <p>1</p>
+      <p>{item.rentalStartDate}</p>
+      <p>{item.rentalEndDate}</p>
+      <S.BtnWrap>
+        <button onClick={() => setModal(true)}>수정</button>
+        <button onClick={open}>삭제</button>
+      </S.BtnWrap>
+    </S.ListLi>
+  )
+}
--- src/components/EquipCartList/index.jsx
+++ src/components/EquipCartList/index.jsx
@@ -1,45 +1,23 @@
-import { useState } from "react"
-import { category } from "../../data/category"
-import Image from "../../modules/Image"
-import ModifyModal from "./ModifyModal"
+import ListItem from "./ListItem"
+import { ListLi } from "./ListItem/style"
 import * as S from "./style"
 
 export default function EquipCartList({cart}) {
-  const [modal, setModal] = useState(false)
-
   return (
-    cart ? 
+    cart && 
     <S.ListUl>
-      <ModifyModal modal={modal} setModal={setModal} />
-      <S.ListLi>
+      <ListLi>
         <p>사진</p>
         <p>기자재</p>
         <p>개수</p>
         <p>수령일</p>
         <p>반납일</p>
         <p> </p>
-      </S.ListLi>
+      </ListLi>
       {
-        cart.map((item, index) => 
-        <S.ListLi key={index}>
-            <Image width="72px" height="72px" borderRadius="10px" src={item.imgUrl} alt='' />
-            <S.ItemWrap>
-              <p>{category.map(value => 
-                  value.value === item.category && value.label
-                  )}</p>
-              <p>{item.modelName}</p>
-            </S.ItemWrap>
-            <p>1</p>
-            <p>{item.rentalStartDate}</p>
-            <p>{item.rentalEndDate}</p>
-            <S.BtnWrap>
-              <button onClick={() => setModal(true)}>수정</button>
-              <button>삭제</button>
-            </S.BtnWrap>
-          </S.ListLi>
-        )
+        cart.map((item) => 
+          <ListItem item={item} key={item.id} />)
       }
-      </S.ListUl>
-      : <p>담은 기자재가 없습니다.</p>
+    </S.ListUl>
   )
 }
