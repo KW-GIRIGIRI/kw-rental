d0e6f60edca1c652da1499c0dd6815a9991cefda
ekdms5566 <ekdms5566@naver.com>
2023-04-28 01:48:33 +0900

    Design: 비밀번호 찾기 페이지 디자인 수정

--- src/modules/Input/style.js
+++ src/modules/Input/style.js
@@ -45,6 +45,22 @@ export const InputStyle = styled.input`
     }
   }
 
+  &.signup {
+    border: 1px solid ${(props) => props.theme.color.gray.g4};
+    border-radius: ${(props) => props.theme.borderRadius.lv1};
+    padding: 11px 15px;
+    font-size: 15px;
+    margin: 10px 0;
+    &::placeholder {
+      font-size: 15px;
+      color: ${(props) => props.theme.color.primary.third};
+    }
+    &:disabled {
+      background-color: ${(props) => props.theme.color.primary.sub};
+      color: ${(props) => props.theme.color.text.gray};
+    }
+  }
+
   &[type="number"] {
     -moz-appearance: textfield;
     &::-webkit-outer-spin-button,
