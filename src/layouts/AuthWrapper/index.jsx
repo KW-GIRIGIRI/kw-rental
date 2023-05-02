4124610d4a623f0b1367926307ce7383d1c2b0af
ekdms5566 <ekdms5566@naver.com>
2023-05-02 15:16:23 +0900

    Feat: 로고 navigate 설정

--- src/layouts/AuthWrapper/index.jsx
+++ src/layouts/AuthWrapper/index.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import Logo from "../../assets/logo.svg"
 import { Section, LogoImg } from "./style"
 
 export default function AuthWrapper() {
+  const navigate = useNavigate()
+
   return (
     <Section>
-      <LogoImg src={Logo} alt="광운대학교 미디어커뮤니케이션학부 랩실 관리 시스템" />
+      <LogoImg src={Logo} alt="광운대학교 미디어커뮤니케이션학부 랩실 관리 시스템" onClick={() => navigate('/auth')} />
       <Outlet />
     </Section>
   )
