eaa15767268beb116a050460f48d3ac1abd5cf67
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-20 23:35:15 +0900

    Feat: 사용자 랩실 대여 대표자 연락망 모달 추가

--- src/components/Modal/style.js
+++ src/components/Modal/style.js
@@ -57,6 +57,39 @@ export const Div = styled.div`
       gap: 12px;
     }
   }
+
+  &.contacts {
+    position: relative;
+    padding: 20px 20px;
+
+    & > button {
+      position: absolute;
+      right: 20px;
+    }
+
+    & > p {
+      margin: 0;
+      position: absolute;
+      font-weight: 600;
+      top: 20px;
+      left: 20px;
+    }
+
+    & > div {
+      display: block;
+      text-align: left;
+      margin-top: 40px;
+
+      & > p:not(:last-child) {
+        margin-bottom: 8px;
+      }
+    }
+
+    & > div:last-child {
+      margin-top: 10px;
+      text-align: right;
+    }
+  }
 `;
 
 export const CloseBtn = styled.button`
