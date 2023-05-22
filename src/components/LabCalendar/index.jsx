067ba261ceae1e8840e3144cb87be1333eefdbef
ekdms5566 <ekdms5566@naver.com>
2023-05-23 03:25:25 +0900

    Feat : 랩실 페이지 - 사용자일 경우 랩실 현황 캘린더 출력 정보 제어
    사용자일 경우, 익일부터 한 달 후까지의 랩실 현황만 출력하도록 설정

--- src/components/LabCalendar/index.jsx
+++ src/components/LabCalendar/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import iconRightArrow from "../../assets/icon-rightArrow.svg";
 import iconLeftArrow from "../../assets/icon-leftArrow.svg";
 import iconCalendar from "../../assets/icon-calendar.svg";
@@ -7,6 +7,7 @@ import * as S from "./style";
 import { useDispatch, useSelector } from "react-redux";
 import { setLabDate } from "../../store/reducer/LabControllerSlice";
 import { getLabRemainQuantities } from "../../api/api";
+import { AuthContext } from "../../context/Context";
 
 const weekDays = ["일", "월", "화", "수", "목", "금", "토"];
 
@@ -16,6 +17,7 @@ const LabCalendar = () => {
   const hanul = useSelector(state => state.labControl.lab)
   const selectDate = useSelector(state => state.labControl.date)
   const dispatch = useDispatch()
+  const { isAuth } = useContext(AuthContext);
 
   const handleGetLabRemain = async () => {
     const startDate = dayObj.startOf('month').format('YYYY-MM-DD')
@@ -52,7 +54,13 @@ const LabCalendar = () => {
   };
 
   const handleSetDate = e => {
-    dispatch(setLabDate(dayjs(`${thisYear}-${thisMonth + 1}-${e.target.value}`).format('YYYY-MM-DD')))
+    if (isAuth) {
+      dispatch(setLabDate(dayjs(`${thisYear}-${thisMonth + 1}-${e.target.value}`).format('YYYY-MM-DD')))
+    } else {
+      const selectDate = dayjs(`${thisYear}-${thisMonth + 1}-${e.target.value}`).valueOf()
+
+      if(selectDate > dayjs().valueOf() && selectDate < dayjs().add(1, 'month').valueOf()) dispatch(setLabDate(dayjs(`${thisYear}-${thisMonth + 1}-${e.target.value}`).format('YYYY-MM-DD')))
+    }
   }
 
   useEffect(() => {
@@ -62,14 +70,20 @@ const LabCalendar = () => {
   return (
     <S.Wrapper>
       <S.Header>
-        <S.NavBtn onClick={handlePrev}>
+        <S.NavBtn
+          onClick={handlePrev}
+          disabled={!isAuth && dayjs().month() === dayObj.month()}
+        >
           <img src={iconLeftArrow} alt="이전 달 보기" />
         </S.NavBtn>
         <S.MonthWrap>
           <img src={iconCalendar} alt="" />
           <span>{dayObj.format("YY년 M월")}</span>
         </S.MonthWrap>
-        <S.NavBtn onClick={handleNext}>
+        <S.NavBtn
+          onClick={handleNext}
+          disabled={!isAuth && dayjs().add(1, 'month').month() === dayObj.month()}
+        >
           <img src={iconRightArrow} alt="다음 달 보기" />
         </S.NavBtn>
       </S.Header>
@@ -104,7 +118,12 @@ const LabCalendar = () => {
             >
               <span>{i + 1}</span>
               {
-                dayjs(`${dayObj.year()}-${dayObj.month() + 1}-${i}`).day() < 4 && (
+                (
+                  dayjs(`${dayObj.year()}-${dayObj.month() + 1}-${i}`).day() < 4 &&
+                  dayjs(`${dayObj.year()}-${dayObj.month() + 1}-${i + 1}`).valueOf() > dayjs().valueOf() &&
+                  dayjs(`${dayObj.year()}-${dayObj.month() + 1}-${i+1}`).valueOf() < dayjs().add(1, 'month').valueOf()
+                )
+                && (
                   dayjs(`${dayObj.year()}-${dayObj.month() + 1}-${i+1}`).format('YYMMDD') === dayjs(selectDate).format('YYMMDD') ?
                   <ins>{hanul ? `대여(${seatArray[i+1] || 16}/16)` : seatArray[i+1] ? "대여 가능" :"대여 불가"}</ins> :
                   <p>{hanul ? `대여(${seatArray[i+1] || 16}/16)` : seatArray[i+1] ? "대여 가능" :"대여 불가"}</p>
