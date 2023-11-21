5337048b148eeda3b5607999c08cb57f2c33a3a5
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-11-21 23:00:58 +0900

    Feat : ga4 추적 코드 구현

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import Router from "./router/Router";
 import { AuthStore } from "./context/Context";
+import ReactGA from "react-ga4";
 
 function App() {
   if (process.env.NODE_ENV === "production") {
     console.log = function no_console() {};
     console.warn = function no_console() {};
     console.error = function no_console() {};
+    ReactGA.initialize(process.env.REACT_APP_GA_ID);
   }
 
   return (
